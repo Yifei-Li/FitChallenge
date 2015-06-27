b687c3c76cc7543501d5e4478b129189caee2102
Yifei-Li <li.yifei9930@gmail.com>
2015-06-26 20:18:06 -0400

    are things working?

--- Android/FitChallenge/app/src/main/java/ca/davidvuong/fitchallenge/MainActivity.java
+++ Android/FitChallenge/app/src/main/java/ca/davidvuong/fitchallenge/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+
+import java.lang.String;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("WTF", "WAAAT");
     }
 
     @Override
